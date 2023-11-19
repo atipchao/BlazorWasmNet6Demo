de940a38700a7342809d0a2f4574f9f093e68bf5
atipchao <atip.chaogunhar@cookcountyil.gov>
2023-11-19 10:48:50 -0600

    Added ProductService to Client Project

---
+++ BlazorWasmNet6Demo/Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorWasmNet6Demo.Client.Services.ProductService
+{
+    public interface IProductService
+    {
+        List<Product> Products { get; set; }
+        Task GetProducts();
+    }
+}
---
+++ BlazorWasmNet6Demo/Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorWasmNet6Demo.Client.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public List<Product> Products { get; set; } = new List<Product>();
+        public HttpClient Http { get; }
+
+        public async Task GetProducts()
+        {
+            var result = 
+                await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
+            if (result != null && result.Data != null)
+            {
+                Products = result.Data;
+            }
+        }
+    }
+}

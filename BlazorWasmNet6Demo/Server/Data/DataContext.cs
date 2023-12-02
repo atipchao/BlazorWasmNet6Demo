cc8aece058e2eb3504e0418de97f5a6c56bef7e8
atipchao <atip.chaogunhar@cookcountyil.gov>
2023-12-02 12:58:13 -0600

    Add/Seed Featured product field to Products table

--- BlazorWasmNet6Demo/Server/Data/DataContext.cs
+++ BlazorWasmNet6Demo/Server/Data/DataContext.cs
@@ -64,9 +64,10 @@ namespace BlazorWasmNet6Demo.Server.Data
                          Id = 1,
                          Title = "The Hitchhiker's Guide to the Galaxy",
                          Description = "The various versions follow the same basic plot but they are in many places mutually contradictory, as Adams rewrote the story substantially for each new adaptation. The various versions follow the same basic plot but they are in many places mutually contradictory, as Adams rewrote the story substantially for each new adaptation. The various versions follow the same basic plot but they are in many places mutually contradictory, as Adams rewrote the story substantially for each new adaptation. The various versions follow the same basic plot but they are in many places mutually contradictory, as Adams rewrote the story substantially for each new adaptation.",
-                         ImageUrl = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
-                         
+                         ImageUrl = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",                         
+                         Featured = true,
                          CategoryId = 1
+
                      },
                     new Product
                     {
@@ -101,6 +102,7 @@ namespace BlazorWasmNet6Demo.Server.Data
                         Title = "Back to the Future",
                         Description = "Back to the Future is a 1985 American science fiction film directed by Robert Zemeckis. Written by Zemeckis and Bob Gale, it stars Michael J. Fox, Christopher Lloyd, Lea Thompson, Crispin Glover, and Thomas F. Wilson. Set in 1985, the story follows Marty McFly (Fox), a teenager accidentally sent back to 1955 in a time-traveling DeLorean automobile built by his eccentric scientist friend Doctor Emmett \"Doc\" Brown (Lloyd). Trapped in the past, Marty inadvertently prevents his future parents' meeting—threatening his very existence—and is forced to reconcile the pair and somehow get back to the future.",
                         ImageUrl = "https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg",
+                        Featured = true
                     },
                     new Product
                     {
@@ -135,6 +137,7 @@ namespace BlazorWasmNet6Demo.Server.Data
                         Title = "Day of the Tentacle",
                         Description = "Day of the Tentacle, also known as Maniac Mansion II: Day of the Tentacle, is a 1993 graphic adventure game developed and published by LucasArts. It is the sequel to the 1987 game Maniac Mansion.",
                         ImageUrl = "https://upload.wikimedia.org/wikipedia/en/7/79/Day_of_the_Tentacle_artwork.jpg",
+                        Featured = true
                     },
                     new Product
                     {
--- BlazorWasmNet6Demo/Shared/Product.cs
+++ BlazorWasmNet6Demo/Shared/Product.cs
@@ -18,6 +18,7 @@ namespace BlazorWasmNet6Demo.Shared
         //public decimal Price { get; set; }
         public Category? Category { get; set; }
         public int CategoryId { get; set; }
+        public bool Featured { get; set; } = false;
         public List<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
     }
 }
